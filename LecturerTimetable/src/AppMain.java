71d6d84a70d16757b5fd7b64e0951f9c10d4786b
ou-ansgar <Ansgar.Goeb@my.open.ac.uk>
2018-04-03 15:20:31 +0200

    added something to the AppMain to get it started

--- LecturerTimetable/src/AppMain.java
+++ LecturerTimetable/src/AppMain.java
@@ -1,9 +1,28 @@
+import main.University;
+import main.UniversityGui;
 
 public class AppMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		University university = new University("UoL Online");
+
+		university.addPerson("First1", "Last1");
+		university.addPerson("First2", "Last2");
+		university.addPerson("First3", "Last3");
+		university.addPerson("First4", "Last4");
+
+		university.addTeacher("TFirst1", "TLast1", "1111", "Prof. IT");
+		university.addTeacher("TFirst2", "TLast2", "2222", "Prof. Law");
+		university.addTeacher("TFirst3", "TLast3", "3333", "Prof. Medicine");
+		university.addTeacher("TFirst4", "TLast4", "4444", "Prof. Physics");
+
+		university.addStudent("SFrist1", "SLast1", "S1111", 1);
+		university.addStudent("SFrist2", "SLast2", "S2222", 2);
+		university.addStudent("SFrist3", "SLast3", "S3333", 3);
+		university.addStudent("SFrist4", "SLast4", "S4444", 2);
+
+		UniversityGui.startUniversity(university);
 	}
 
 }
