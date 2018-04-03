7ee1bcaf135aff5344e20f6cdabc32c27288b12e
Helmi Hasan <helmi.baraja@gmail.com>
2018-04-03 23:35:23 +0800

    added ID counter on Course

--- LecturerTimetable/src/main/Course.java
+++ LecturerTimetable/src/main/Course.java
@@ -4,12 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Course extends CourseBase{
+	
+	private int idCOunter= 0;
+	private List<CourseModel> coursesList = new ArrayList<>();
 
-	List<CourseModel> coursesList = new ArrayList<>();
+	public List<CourseModel> getCoursesList() {
+		return coursesList;
+	}
+
+	public void setCoursesList(List<CourseModel> coursesList) {
+		this.coursesList = coursesList;
+	}
+
 	
 	@Override
 	public void addOnlineCourse(CourseModel courseModel) {
 		// TODO Auto-generated method stub
+		idCOunter++;
+		courseModel.setId(idCOunter);
 		courseModel.setOnlineCourse(true);
 		coursesList.add(courseModel);
 	}
@@ -17,6 +29,8 @@ public class Course extends CourseBase{
 	@Override
 	public void addOfflineCourse(CourseModel courseModel) {
 		// TODO Auto-generated method stub
+		idCOunter++;
+		courseModel.setId(idCOunter);
 		courseModel.setOnlineCourse(false);
 		coursesList.add(courseModel);
 	}
